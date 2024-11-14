0047372832a3ce5008a5c044a0e356d086773d7e
harshulvijay <64775657+harshulvijay@users.noreply.github.com>
2024-11-14 23:16:09 +0530

    feat: add Express-based API for logging keystrokes
    add API that facilitates logging of keystrokes and saving them to a Firebase Firestore database

---
+++ server/esbuild.config.js
@@ -0,0 +1,44 @@
+import esbuild from "esbuild";
+import esbuildPluginTsc from "esbuild-plugin-tsc";
+import { NodeResolvePlugin as NodeResolve } from "@esbuild-plugins/node-resolve";
+
+/**
+ * @param {import "esbuild".BuildOptions} options
+ * @returns {import "esbuild".BuildOptions}
+ */
+function createBuildSettings(options) {
+	return {
+		entryPoints: ["src/index.ts"],
+		outfile: "dist/server/index.js",
+		plugins: [
+			esbuildPluginTsc({
+				force: true,
+				tsconfigPath: "./tsconfig.json",
+			}),
+			NodeResolve({
+				extensions: [".ts", ".js"],
+				onResolved: (resolved) => {
+					if (resolved.includes("node_modules")) {
+						return {
+							external: true,
+						};
+					}
+					return resolved;
+				},
+			}),
+		],
+		...options,
+	};
+}
+
+const options = createBuildSettings({
+	bundle: true,
+	minify: true,
+	minifySyntax: true,
+	format: "esm",
+	treeShaking: true,
+	platform: "node",
+	sourcemap: true,
+});
+
+esbuild.build(options);
---
+++ server/src/routes/ws.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+// router
+const router = Router({});
+
+router.route("/ws").get();
+
+export default router;
