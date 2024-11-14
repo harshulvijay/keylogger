4f37fabb1b066434d5a84378dc518b63aff4deaa
harshulvijay <64775657+harshulvijay@users.noreply.github.com>
2024-11-15 00:11:51 +0530

    chore: move `index.ts` to `server/src` from `server/api`

--- server/esbuild.config.js
+++ server/esbuild.config.js
@@ -8,7 +8,7 @@ import { NodeResolvePlugin as NodeResolve } from "@esbuild-plugins/node-resolve"
  */
 function createBuildSettings(options) {
 	return {
-		entryPoints: ["api/index.ts"],
+		entryPoints: ["src/index.ts"],
 		outfile: "dist/server/index.js",
 		plugins: [
 			esbuildPluginTsc({
